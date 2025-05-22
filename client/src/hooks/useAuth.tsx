3c89fdd06e850b3a1a9c9931e0e1bc2f170cdbc5
Dwxrdel <piskynov04@mail.ru>
2025-05-22 16:13:16 +0300

    1234

--- client/src/hooks/useAuth.tsx
+++ client/src/hooks/useAuth.tsx
@@ -1,7 +1,7 @@
 // @/hooks/useAuth.tsx
 import { useContext } from 'react';
 import { AuthContext } from '@/contexts/AuthContext';
-import type { AuthContextType } from '@/types/auth';
+import type { AuthContextType } from '@/types/auth.ts';
 
 export function useAuth(): AuthContextType {
   const context = useContext(AuthContext);
---
+++ client/types/auth.ts
@@ -0,0 +1,10 @@
+export type User = {
+  id: number;
+  username: string;
+  role: string;
+  firstName: string;
+  lastName: string;
+  middleName?: string;
+  groupId?: number;
+  departmentId?: number;
+};
